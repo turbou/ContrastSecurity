d33f4fb78ab48f782b3176cf84d44a22017f6e9a
turbou <turbou@i.softbank.jp>
2025-03-06 14:02:20 +0900

    update.

--- support/statistics03/collector.bat
+++ support/statistics03/collector.bat
@@ -1,4 +1,4 @@
 @echo off
-call "C:\Users\turbou\git\ContrastSecurity\support\statistics03\sample_venv\Scripts\activate"
+call "C:\Users\turbou\git\ContrastSecurity\support\statistics03\.sample_venv\Scripts\activate"
 python C:\Users\turbou\git\ContrastSecurity\support\statistics03\collector.py --app_filter PetClinic_8001_Taka
 deactivate
